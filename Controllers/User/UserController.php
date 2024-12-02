4ed85cdb62e66c51fd7b9415fd85ca4f6b0c80a3
IvanAlexisUrbina <alexis-crokis@hotmial.com>
2024-12-01 22:56:03 -0500

    feat: Implementado UserController para gestionar los usuarios y sus roles

---
+++ Controllers/User/UserController.php
@@ -0,0 +1,22 @@
+<?php
+require '../vendor/autoload.php';
+use Models\User\UserModel;
+use function Helpers\dd;
+use function Helpers\generateUrl;
+use function Helpers\redirect;
+
+class UserController
+{
+ 
+    
+    
+    
+   
+}
+
+
+
+
+
+
+?>
