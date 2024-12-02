431998ddd18802dc76447417795083a0f5268c68
IvanAlexisUrbina <alexis-crokis@hotmial.com>
2024-12-01 22:57:11 -0500

    feat: Añadida vista list.php para mostrar la lista de libros disponibles

---
+++ Views/book/list.php
@@ -0,0 +1,82 @@
+<div class="container mt-5">
+    <h2>Listado de Libros</h2>
+
+    <?php if ($_SESSION['RolUser']!='4') {?>
+    <button id="BookCreate" data-url="<?= Helpers\generateUrl("Book","Book","viewCreateBook",[],"ajax")?>"
+        class="btn btn-dark mb-4">
+        Registrar Libros
+    </button>
+    <?php }?>
+
+
+    <!-- Tabla con DataTable -->
+    <table id="reservationsTable" class="DataTable table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>ISBN</th>
+                <th>Editorial</th>
+                <th>Año de Publicación</th>
+                <th>Cantidad</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data as $book) {
+                // Verificar si el usuario tiene el rol 4 y el libro está inactivo
+                if ($_SESSION['RolUser'] == '4' && $book['status'] == 'Inactivo') {
+                    continue; // Saltar esta iteración del bucle
+                }
+            ?>
+                <tr>
+                    <td><?= $book['title'] ?></td>
+                    <td><?= $book['author'] ?></td>
+                    <td><?= $book['isbn'] ?></td>
+                    <td><?= $book['publisher'] ?></td>
+                    <td><?= $book['publication_year'] ?></td>
+                    <td><?= $book['quantity'] ?></td>
+                    <td>
+                        <?php
+                        $status = $book['status'];
+                        $color = '';
+                        switch ($status) {
+                            case 'Activo':
+                                $color = 'green'; // verde
+                                break;
+                            case 'Inactivo':
+                                $color = 'red'; // rojo
+                                break;
+                            case 'Sin inventario':
+                                $color = 'grey'; // gris
+                                break;
+                            default:
+                                $color = '';
+                                break;
+                        }
+                        ?>
+                        <span style="color: <?= $color ?>"><?= ucfirst($status) ?></span>
+                    </td>
+                    <td>
+                        <div class="btn-group">
+                            
+                                 <button class="btn btn-warning" id="BookRent" data-url="<?= Helpers\generateUrl("Book", "Book", "viewModalRentBook", ['b_id' => $book['id']], "ajax") ?>">
+                                    <i class="fas fa-book-reader"></i> Alquilar
+                                </button>
+                            <?php if ($_SESSION['RolUser'] != '4') { ?>
+                                <button class="btn btn-success" id="BookUpdate" data-url="<?= Helpers\generateUrl("Book", "Book", "viewModalBook", ['b_id' => $book['id']], "ajax") ?>">
+                                    <i class="fas fa-book-reader"></i> Editar
+                                </button>
+                                <button class="btn btn-primary" id="BookStatus" data-url="<?= Helpers\generateUrl("Book", "Book", "viewModalStatusBook", ['b_id' => $book['id']], "ajax") ?>">
+                                    <i class="fas fa-book-reader"></i> Cambiar Estado
+                                </button>
+                            <?php } ?>
+                        </div>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+<!-- <?=var_dump($data);?> -->
